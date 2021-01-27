2fc6490c3d2853a42918169672973a909bac2f5a
Muskan-1527 <agrawalm883@gmail.com>
2021-01-27 23:35:44 +0530

    creating and displaying comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,12 +7,20 @@ module.exports.home = function(req, res){
     //         posts: posts
     //     });
     // });
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
-            posts: posts
+            posts:  posts
         });
-    });
+    })
 }
 
 //module.exports..actionName = function(req,res){}
