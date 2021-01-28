6a1339e0d34cc345ed5da749ce60f9ef4c7b713d
Muskan-1527 <agrawalm883@gmail.com>
2021-01-28 13:47:52 +0530

    upate user profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
     // Post.find({}, function(err, posts){
@@ -16,11 +17,15 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
         });
-    })
+    });
 }
 
 //module.exports..actionName = function(req,res){}
